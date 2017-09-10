15c64b2b60d99b7b4bd1304a9cdeb0a35b665db4
David7000 <dor@orange.fr>
2017-09-10 12:43:33 +0200

    Paramètres de connexion sur OpenShift

--- src/main/java/com/test/util/ServerDataBase.java
+++ src/main/java/com/test/util/ServerDataBase.java
@@ -8,19 +8,19 @@ public class ServerDataBase {
    // Database Name: test_mvc_crud 
    // Connection URL: mysql://mysql:3306/ 
     
-    //public static final String DATABASE = "jdbc:mysql://mysql:3306/test_mvc_crud";
+    public static final String DATABASE = "jdbc:mysql://mysql:3306/test_mvc_crud";
     
      //public static final String DATABASE = "jdbc:mysql://10.130.126.129:3306/test_mvc_crud";
    
     
-  public static final String DATABASE = "jdbc:mysql://127.0.0.1:3306/test_mvc_crud";
+  //public static final String DATABASE = "jdbc:mysql://127.0.0.1:3306/test_mvc_crud";
 
     
-     // public static final String LOGIN = "adminskllvJ7";
-   public static final String LOGIN = "root";
+    public static final String LOGIN = "adminskllvJ7";
+   //public static final String LOGIN = "root";
 
     
-     // public static final String PASSWORD = "VHz5X3422RDV";
-  public static final String PASSWORD = "";
+    public static final String PASSWORD = "VHz5X3422RDV";
+  //public static final String PASSWORD = "";
 
 }
